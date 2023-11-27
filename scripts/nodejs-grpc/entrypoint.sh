572a0b5d5d2fdb260f7803e56600cb719f126e2c
Ahmet Karapinar <ahmetkca@ahmetkca-Ubuntu-23.10>
2023-11-27 04:47:35 -0500

    - Removed datastore_service.proto and metadata_service.proto files from go-gprc, nodejs-grpc, and python-grpc directories, indicating a restructuring or removal of these gRPC service definitions. - Added .gitkeep files in go-gprc/proto, nodejs-grpc/proto, and python-grpc/proto directories, possibly to maintain the directory structure in the absence of .proto files. - In the nodejs-grpc Dockerfile, removed installation of grpc-tools and replaced it with the installation of ts-proto, suggesting a shift in the protocol buffer compilation strategy from grpc-tools to ts-proto for TypeScript support. - Modified the entrypoint.sh script in nodejs-grpc to use protoc with the ts_proto plugin, aligning with the change to ts-proto in the Dockerfile. This adjustment indicates a switch from JavaScript to TypeScript for gRPC service generation. - The overall changes suggest a significant refactoring of the gRPC services setup across multiple language implementations (Go, Node.js, Python), with a focus on TypeScript compatibility in the Node.js implementation.
    These changes should be reviewed to ensure they align with the intended architecture and dependency management strategy.

--- scripts/nodejs-grpc/entrypoint.sh
+++ scripts/nodejs-grpc/entrypoint.sh
@@ -1,10 +1,23 @@
 #!/bin/sh
 # Iterate over each .proto file and generate gRPC files for Node.js
 for proto_file in ${PROTO_FILES_DIR}/*.proto; do
-  grpc_tools_node_protoc \
-    --js_out=import_style=commonjs,binary:/generated \
-    --grpc_out=grpc_js:/generated \
-    --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
-    -I${PROTO_FILES_DIR} \
-    "${proto_file}"
+  # grpc_tools_node_protoc \
+  #   --js_out=import_style=commonjs,binary:/generated \
+  #   --grpc_out=grpc_js:/generated \
+  #   --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
+  #   -I${PROTO_FILES_DIR} \
+  #   "${proto_file}"
+  
+  # protoc \
+  #   --plugin=$(npm root)/.bin/protoc-gen-ts_proto \
+  #   --ts_proto_out=/generated \
+  #   --ts_proto_opt=outputServices=generic-definitions,outputClientImpl=false,oneof=unions,snakeToCamel=false,esModuleInterop=true \
+  #   --proto_path=${PROTO_FILES_DIR} \
+  #   "${proto_file}"
+
+  protoc --plugin=$(npm root)/.bin/protoc-gen-ts_proto \
+    --ts_proto_out=/generated \
+    --ts_proto_opt=outputServices=grpc-js \
+    --ts_proto_opt=esModuleInterop=true \
+    -I=${PROTO_FILES_DIR} ${proto_file}
 done
