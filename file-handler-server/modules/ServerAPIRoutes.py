fca2967ad9b1ab2613b4d15b1e0c336c55f7304f
Ahmet Karapinar <ahmetkca@ahmetkca-Ubuntu-23.10>
2023-11-28 00:30:17 -0500

    Refactor ServerAPIRoutes.py to Support Multiple File Uploads and Improve Error Responses
    This commit introduces a significant enhancement to the file upload functionality within the file-handler-server. The previous implementation in ServerAPIRoutes.py only supported the upload of a single file at a time, and error responses were plain text. With this update, the endpoint now supports multiple file uploads and returns error messages in JSON format, aligning with RESTful API best practices.
    
    Changes:
    1. Modified the '/upload' route to accept multiple files. The request.files key 'file' has been changed to 'files[]' to handle an array of files.
    2. Implemented a check for the presence of 'files[]' in the request. If absent, the server now returns a JSON object with an error message, enhancing the API's usability and client-side error handling.
    3. Added validation to ensure that each file in the 'files[]' array has a filename. This prevents processing empty file fields, improving the robustness of the file upload process.
    4. Altered the response structure for successful uploads. Now, the server returns a JSON object containing a list of uploaded files, each with an 'id' and 'name'. This aids clients in tracking multiple file uploads.
    5. Added TODO comments for future enhancements, indicating areas where the integration with metadata and datastore services is planned.
    
    This refactor not only improves the functionality but also makes the API more intuitive and robust, aligning with modern web standards. The use of JSON for error handling greatly enhances the API's client compatibility and ease of integration.
    
    Note: The commit includes no changes to the file download functionality or other parts of the ServerAPIRoutes.py module.

--- file-handler-server/modules/ServerAPIRoutes.py
+++ file-handler-server/modules/ServerAPIRoutes.py
@@ -2,7 +2,7 @@ import json
 import os
 import uuid
 import Config
-from flask import Blueprint, request, send_file
+from flask import Blueprint, jsonify, request, send_file
 from modules.FileSegmentation import split_file, reassemble_file
 from modules.FileTransmission import send_segment_to_ip, receive_segment_from_ip
 from modules.ServerCommunication import get_ip_list, get_ips_for_uuid
@@ -11,22 +11,37 @@ server_routes = Blueprint('server_routes', __name__)
 
 @server_routes.route('/upload', methods=['POST'])
 def upload_file():
-    if 'file' not in request.files:
-        return "No file part", 400
-    file = request.files['file']
-    if file.filename == '':
-        return "No selected file", 400
-    if file:
-        unique_id = str(uuid.uuid4())
-        file_path = os.path.join(Config.UPLOAD_FOLDER, unique_id)
-        file.save(file_path)
-        split_file(file_path, Config.DOWNLOAD_FOLDER, num_segments=Config.num_segments)
-        destination_ips = get_ip_list(Config.num_segments)
-        ips_list = destination_ips['ips']
-        for i, ip in enumerate(ips_list):
-            segment_name = f"{unique_id}_segment_{i + 1}.dat"
-            send_segment_to_ip(segment_name=segment_name, destination_ip=ip)
-        return f"File uploaded with ID: {unique_id}", 200
+    if 'files[]' not in request.files:
+        return jsonify({"error": "No files part"}), 400
+
+    files = request.files.getlist('files[]')
+
+    if not files or any(file.filename == '' for file in files):
+        return jsonify({"error": "No selected file"}), 400
+    
+    uploaded_files = []
+    for file in files:
+        if file:
+            unique_id = str(uuid.uuid4())
+            file_path = os.path.join(Config.UPLOAD_FOLDER, unique_id)
+            file.save(file_path)
+            split_file(file_path, Config.DOWNLOAD_FOLDER, num_segments=Config.num_segments)
+
+            # TODO: get list of destination ips from metadata service
+            destination_ips = get_ip_list(Config.num_segments)
+
+            ips_list = destination_ips['ips']
+            for i, ip in enumerate(ips_list):
+                segment_name = f"{unique_id}_segment_{i + 1}.dat"
+
+                # TODO: send segment to destination ip using datastore service
+                send_segment_to_ip(segment_name=segment_name, destination_ip=ip)
+
+            uploaded_files.append({
+                "id": unique_id,
+                "name": file.filename
+            })
+    return jsonify({"files": uploaded_files}), 200
 
 @server_routes.route('/download/<file_id>', methods=['GET'])
 def download_file(file_id):
