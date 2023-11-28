3ae19c56271f9bd1247f7781369ebc78f852e843
Ahmet Karapinar <ahmetkca@ahmetkca-Ubuntu-23.10>
2023-11-28 00:09:27 -0500

    Refactor file name extraction in FileSegmentation module
    This commit refactors the method used to extract the file name in the
    split_file function of the FileSegmentation module. Previously, the
    file name was obtained by splitting the input_file string on '\\', which
    could lead to issues on platforms where a different file separator is
    used (e.g., POSIX systems).
    
    The updated implementation utilizes os.path.split(input_file)[-1] to
    extract the file name. This change ensures cross-platform compatibility
    by correctly handling different file separators across operating systems.

--- file-handler-server/modules/FileSegmentation.py
+++ file-handler-server/modules/FileSegmentation.py
@@ -17,7 +17,7 @@ def split_file(input_file, output_directory, num_segments):
         file_size = os.path.getsize(input_file)
         segment_size = file_size // num_segments
         for i in range(num_segments):
-            file_name = input_file.split('\\')[-1]
+            file_name = os.path.split(input_file)[-1]
             segment_name = f"{file_name}_segment_{i + 1}.dat"
             output_path = os.path.join(output_directory, segment_name)
             with open(output_path, 'wb') as outfile:
