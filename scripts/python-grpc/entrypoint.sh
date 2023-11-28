79331b4d4b123d0d1840fd0a9a0ed18de90dcc5c
ahmetkca <ahmet.karapinar@ontariotechu.net>
2023-11-28 17:24:54 -0500

    Optimize Python gRPC script to generate .pyi stub files
    This commit updates the Python gRPC entrypoint script to enhance the
    code generation process. Specifically, it modifies the protoc command
    used in the script to generate .pyi stub files in addition to the
    existing Python and gRPC outputs. These stub files are essential for
    improved type checking and editor support, contributing to better
    code quality and developer experience.
    
    Changes:
    - Added the --pyi_out flag to the protoc command.
    - Ensured that .pyi files are generated in the /generated directory
      alongside Python and gRPC code.

--- scripts/python-grpc/entrypoint.sh
+++ scripts/python-grpc/entrypoint.sh
@@ -3,5 +3,5 @@
 mkdir -p /generated
 
 for proto in ${PROTO_FILES_DIR}/*.proto; do
-    python -m grpc_tools.protoc -I${PROTO_FILES_DIR} --python_out=/generated --grpc_python_out=/generated "$proto"
+    python -m grpc_tools.protoc -I${PROTO_FILES_DIR} --python_out=/generated --grpc_python_out=/generated --pyi_out=/generated "$proto"
 done
